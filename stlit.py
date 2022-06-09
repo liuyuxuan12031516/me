1cd06caf88a24fe8ec6f8c3a0a2f514879917e48
liuyuxuan12031516 <86498670+liuyuxuan12031516@users.noreply.github.com>
2022-06-09 09:13:53 +0800

    Update stlit.py

--- stlit.py
+++ stlit.py
@@ -15,51 +15,69 @@ from scipy.fft import fft
 
 st.title('言语语音可视化平台')
 
-uploaded_file1 = st.file_uploader("Choose a WAV file", type="wav")
-uploaded_file2 = st.file_uploader("Choose a WAV file", type="wav")
+import streamlit as st
+import base64
+from pathlib import Path
+import tempfile
 
-if uploaded_file1 is not None:
-    sample_rate,signal=scipy.io.wavfile.read(uploaded_file)
+def writer():
+    file = st.file_uploader("选择待上传的PDF文件", type=['pdf'])
+    if st.button("点击"):
+        if file is not None:
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                fp = Path(tmp_file.name)
+                fp.write_bytes(file.getvalue())
+                with open(tmp_file.name, "rb") as f:
+                    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+                pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" ' \
+                              f'width="800" height="1000" type="application/pdf">'
+                st.markdown(pdf_display, unsafe_allow_html=True)
 
-# file = st.sidebar.selectbox(
-#         'file',
-#         ['D:/桌面/CGZ-speaker1-a-T1.wav','D:/桌面/ZQ-speaker1-a-T4.wav'])
+# uploaded_file1 = st.file_uploader("Choose a WAV file", type="wav")
+# uploaded_file2 = st.file_uploader("Choose a WAV file", type="wav")
 
-'file:',uploaded_file1
-option = st.sidebar.selectbox(
-        'option',
-        ['time domain','mfcc','frequency domain'])
-'option:', option
+# if uploaded_file1 is not None:
+#     sample_rate,signal=scipy.io.wavfile.read(uploaded_file)
 
-if option == 'time domain':
-    fig1 = plt.figure()
-    plt.plot(signal)
-    plt.title(option)
-    st.pyplot(fig1)
+# # file = st.sidebar.selectbox(
+# #         'file',
+# #         ['D:/桌面/CGZ-speaker1-a-T1.wav','D:/桌面/ZQ-speaker1-a-T4.wav'])
 
-elif option == 'mfcc':
-    feature_mfcc = mfcc(signal, samplerate=sample_rate,nfft=1103,winfunc=np.hamming)
-    fig2 = plt.figure()
-    mfcc_data= np.swapaxes(feature_mfcc, 0 ,1)
-    plt.imshow(mfcc_data, interpolation='nearest', cmap=cm.coolwarm, origin='lower')
-    plt.title(option)
-    st.pyplot(fig2)
+# 'file:',uploaded_file1
+# option = st.sidebar.selectbox(
+#         'option',
+#         ['time domain','mfcc','frequency domain'])
+# 'option:', option
 
-else:
-    fig3 = plt.figure()
-    x, sr = librosa.load(file, sr=16000)
-    print(len(x))
-    ft = fft(x)
-    print(len(ft), type(ft), np.max(ft), np.min(ft))
-    magnitude = np.absolute(ft)  # 对fft的结果直接取模（取绝对值），得到幅度magnitude
-    frequency = np.linspace(0, sr, len(magnitude))  # (0, 16000, 121632)
-    # plot spectrum，限定[:40000]
-    # plt.figure(figsize=(18, 8))
-    plt.plot(frequency[:40000], magnitude[:40000])  # magnitude spectrum
-    plt.title(option)
-    plt.xlabel("Hz")
-    plt.ylabel("Magnitude")
-    st.pyplot(fig3)
+# if option == 'time domain':
+#     fig1 = plt.figure()
+#     plt.plot(signal)
+#     plt.title(option)
+#     st.pyplot(fig1)
+
+# elif option == 'mfcc':
+#     feature_mfcc = mfcc(signal, samplerate=sample_rate,nfft=1103,winfunc=np.hamming)
+#     fig2 = plt.figure()
+#     mfcc_data= np.swapaxes(feature_mfcc, 0 ,1)
+#     plt.imshow(mfcc_data, interpolation='nearest', cmap=cm.coolwarm, origin='lower')
+#     plt.title(option)
+#     st.pyplot(fig2)
+
+# else:
+#     fig3 = plt.figure()
+#     x, sr = librosa.load(file, sr=16000)
+#     print(len(x))
+#     ft = fft(x)
+#     print(len(ft), type(ft), np.max(ft), np.min(ft))
+#     magnitude = np.absolute(ft)  # 对fft的结果直接取模（取绝对值），得到幅度magnitude
+#     frequency = np.linspace(0, sr, len(magnitude))  # (0, 16000, 121632)
+#     # plot spectrum，限定[:40000]
+#     # plt.figure(figsize=(18, 8))
+#     plt.plot(frequency[:40000], magnitude[:40000])  # magnitude spectrum
+#     plt.title(option)
+#     plt.xlabel("Hz")
+#     plt.ylabel("Magnitude")
+#     st.pyplot(fig3)
 
 
 
