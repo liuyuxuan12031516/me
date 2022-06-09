aca403f9308fc833ac28c5e407f7e951060634d8
liuyuxuan12031516 <86498670+liuyuxuan12031516@users.noreply.github.com>
2022-06-09 08:58:05 +0800

    Update stlit.py

--- stlit.py
+++ stlit.py
@@ -1,4 +1,3 @@
-import librosa
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -17,21 +16,22 @@ from scipy.fft import fft
 
 st.title('言语语音可视化平台')
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="wav")
+uploaded_file1 = st.file_uploader("Choose a CSV file", type="wav")
+uploaded_file2 = st.file_uploader("Choose a CSV file", type="wav")
 
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     st.write(data)
 
-file = st.sidebar.selectbox(
-        'file',
-        ['D:/桌面/CGZ-speaker1-a-T1.wav','D:/桌面/ZQ-speaker1-a-T4.wav'])
+# file = st.sidebar.selectbox(
+#         'file',
+#         ['D:/桌面/CGZ-speaker1-a-T1.wav','D:/桌面/ZQ-speaker1-a-T4.wav'])
 'file:',file
 option = st.sidebar.selectbox(
         'option',
         ['time domain','mfcc','frequency domain'])
 'option:', option
-sample_rate,signal=scipy.io.wavfile.read(file)
+sample_rate,signal=scipy.io.wavfile.read(uploaded_file)
 if option == 'time domain':
     fig1 = plt.figure()
     plt.plot(signal)
